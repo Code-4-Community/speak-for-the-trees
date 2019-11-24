baf227c97a011af1170bfbc4a9e684d85e5e5b02
ZachOBrien <zachobrien99@gmail.com>
2019-11-24 14:46:24 -0500

    navbar work in progress

---
+++ src/components/nav/TheNavbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="header-container">
+      <div class="header__hamburger-menu">
+          <hamburger-menu />
+      </div>
+      <div class="header__logo">logo</div>
+  </div>
+</template>
+
+<script>
+import HamburgerMenu from '../menus/HamburgerMenu.vue';
+
+export default {
+  name: 'Header',
+  components: {
+    HamburgerMenu,
+  },
+};
+</script>
+
+<style>
+.header-container {
+    padding: 1rem;
+    width: 95vw;
+    height: 3rem;
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.header__logo {
+    height: 1rem;
+    width: 1rem;
+    color: white;
+    background-color: grey;
+    margin-right: 1rem;
+}
+</style>
