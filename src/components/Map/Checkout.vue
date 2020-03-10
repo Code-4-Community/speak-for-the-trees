d2daec4109301d4d4ade8f0e15eb00bfc5f68865
Bill <wohlers.w@husky.neu.edu>
2020-03-09 20:39:27 -0400

    started work

---
+++ src/components/Map/Checkout.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <table>
+      <tr v-for="street in streets" :key="street.id">
+        <td>{{ street.name }}</td>
+        <td>Remove</td>
+      </tr>
+    </table>
+
+    <button>Checkout</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Checkout',
+  computed: {
+    streets() {
+      return [
+        {
+          id: 0,
+          name: 'Street 1',
+        },
+        {
+          id: 1,
+          name: 'Street 2',
+        },
+      ];
+    },
+  },
+};
+</script>
