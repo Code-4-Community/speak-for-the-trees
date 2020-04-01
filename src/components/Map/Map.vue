d45d61481bb1ef5249fadac5237b4aae3f8b95fa
gabriel-peter <peter.g@husky.neu.edu>
2020-04-01 14:24:45 -0700

    Readded selection option

--- src/components/Map/Map.vue
+++ src/components/Map/Map.vue
@@ -8,35 +8,56 @@ import { loadModules } from 'esri-loader';
 export default {
   name: 'map',
   mounted() {
+    const reserveSegment = {
+      title: 'Reserve',
+      id: 'reserve-this',
+      image: 'https://developers.arcgis.com/javascript/latest/sample-code/popup-actions/live/Measure_Distance16.png',
+    };
+    const template = {
+      // autocasts as new PopupTemplate()
+      title: '{ST_NAME} {ST_TYPE}', // Show attribute value
+      content: '<b>ID:</b> {FID} <strong>RESERVED: {RESERVED}</strong>',
+      actions: [reserveSegment],
+    };
+    const renderer = {
+      // https://developers.arcgis.com/javascript/latest/api-reference/esri-layers-FeatureLayer.html#renderer
+      // https://developers.arcgis.com/javascript/latest/api-reference/esri-renderers-UniqueValueRenderer.html
+      type: 'unique-value',
+      field: 'ST_TYPE',
+      defaultSymbol: { type: 'simple-line' },
+      uniqueValueInfos: [{
+        value: 'ST',
+        symbol: {
+          type: 'simple-line', // autocasts as new SimpleFillSymbol()
+          color: 'blue',
+          width: '3px',
+        },
+      }, {
+        value: 'AVE',
+        symbol: {
+          type: 'simple-line', // autocasts as new SimpleFillSymbol()
+          color: 'purple',
+          width: '1px',
+        },
+      },
+      ],
+    };
+    const sqlExpressions = ['*', "ST_TYPE = 'ST'", "ST_TYPE = 'AVE'", "ST_TYPE = 'PL'"];
+    const selectFilter = document.createElement('select');
+    selectFilter.setAttribute('class', 'esri-widget esri-select');
+    selectFilter.setAttribute('style', 'width: 275px; font-family: Avenir Next W00; font-size: 1em');
+    sqlExpressions.forEach((sql) => {
+      const option = document.createElement('option');
+      option.value = sql;
+      option.innerHTML = sql;
+      selectFilter.appendChild(option);
+    });
     // lazy load the required ArcGIS API for JavaScript modules and CSS
     loadModules(['esri/Map', 'esri/views/MapView', 'esri/layers/FeatureLayer'], { css: true })
       .then(([ArcGISMap, MapView, FeatureLayer]) => {
         const map = new ArcGISMap({
           basemap: 'gray',
         });
-        const renderer = {
-          // https://developers.arcgis.com/javascript/latest/api-reference/esri-layers-FeatureLayer.html#renderer
-          // https://developers.arcgis.com/javascript/latest/api-reference/esri-renderers-UniqueValueRenderer.html
-          type: 'unique-value',
-          field: 'ST_TYPE',
-          defaultSymbol: { type: 'simple-line' },
-          uniqueValueInfos: [{
-            value: 'ST',
-            symbol: {
-              type: 'simple-line', // autocasts as new SimpleFillSymbol()
-              color: 'blue',
-              width: '3px',
-            },
-          }, {
-            value: 'AVE',
-            symbol: {
-              type: 'simple-line', // autocasts as new SimpleFillSymbol()
-              color: 'purple',
-              width: '1px',
-            },
-          },
-          ],
-        };
         this.view = new MapView({
           container: this.$el,
           map,
@@ -47,8 +68,16 @@ export default {
           url: 'https://services7.arcgis.com/iIw2JoTaLFMnHLgW/ArcGIS/rest/services/boston_street_segments_1/FeatureServer/0',
           renderer,
           outFields: ['SEGMENT_ID', 'ST_NAME'],
+          popupTemplate: template,
           // https://developers.arcgis.com/javascript/latest/api-reference/esri-PopupTemplate.html
           // popupTemplate: template,
+        });
+        this.view.ui.add(selectFilter, 'bottom-right');
+        function setFeatureLayerFilter(expression) {
+          streetSegments.definitionExpression = expression;
+        }
+        selectFilter.addEventListener('change', (event) => {
+          setFeatureLayerFilter(event.target.value);
         });
         map.add(streetSegments);
       });
@@ -66,6 +95,7 @@ export default {
 
 <style scoped>
 .map {
+  margin-left: 15%;
   width: 70vw;
   height: 70vh;
 }
