e68ead00ca2eaf6504422d2b2d76ddfcbd36d9e1
florisdobber <florisdobber@gmail.com>
2020-04-24 12:27:01 +0200

    Fixed suggested changes

---
+++ src/components/Leaderboard/My progress.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="progress" v-if="!showPlayers">
+        <h2>Team 3</h2>
+        <img alt="Icon">
+        <p class="ranking">3rd place</p>
+        <p class="points">20</p>
+        <div class="members">
+          <p>Member 1</p>
+          <p>Member 2</p>
+          <p>Member 3</p>
+          <p>Member 4</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MyProgress',
+};
+</script>
+
+<style scoped lang="less">
+.progress h2 {
+  position: absolute;
+  top: 14vh;
+  left: 35vw;
+}
+
+.progress .ranking {
+  position: absolute;
+  top: 20vh;
+  left: 35vw;
+}
+
+.progress .points {
+  position: absolute;
+  top: 15vh;
+  left: 72vw;
+  font-style: italic;
+}
+
+.progress img {
+  position: absolute;
+  top: 15vh;
+  left: 8vw;
+}
+
+.progress .members {
+  position: absolute;
+  top: 35vh;
+  left: 8vw;
+  p {
+    margin: 0.5em 0;
+  }
+}
+</style>
--- src/views/Leaderboard.vue
+++ src/views/Leaderboard.vue
@@ -4,61 +4,34 @@
     <div class="board">
       <!-- heading to select view -->
       <div class="heading">
-        <button class="active" @click="openPlayers" v-if="showPlayers">Players</button>
-        <button class="inactive" @click="openPlayers" v-if="!showPlayers">Players</button>
+        <button
+        v-bind:class="[showPlayers ? 'active' : 'inactive']"
+        @click="openPlayers">
+        Players
+        </button>
         <div class=divider></div>
-        <button class="active" @click="openProgress" v-if="!showPlayers">My progress</button>
-        <button class="inactive" @click="openProgress" v-if="showPlayers">My progress</button>
+        <button
+        v-bind:class="[showPlayers ? 'inactive' : 'active']"
+        @click="openProgress">
+        My Progress
+        </button>
       </div>
-      <!-- Players -->
-      <div class="rows" v-if="showPlayers">
-        <!-- replace with v-for once data is loaded in -->
-        <div class="row">
-          <p class="rank">1</p>
-          <p>Team 1</p>
-          <p class="points">25</p>
-        </div>
-        <div class="row">
-          <p class="rank">2</p>
-          <p>Team 2</p>
-          <p class="points">22</p>
-        </div>
-        <div class="row">
-          <p class="rank">3</p>
-          <p>Team 3</p>
-          <p class="points">20</p>
-        </div>
-        <div class="row">
-          <p class="rank">4</p>
-          <p>Team 4</p>
-          <p class="points">14</p>
-        </div>
-        <div class="row">
-          <p class="rank">5</p>
-          <p>Team 5</p>
-          <p class="points">10</p>
-        </div>
-      </div>
-      <!-- My progress -->
-      <div class="progress" v-if="!showPlayers">
-        <h2>Team 3</h2>
-        <img src="../assets/tree 2.png" alt="Icon">
-        <p class="ranking">3rd place</p>
-        <p class="points">20</p>
-        <div class="members">
-          <p>Member 1</p>
-          <p>Member 2</p>
-          <p>Member 3</p>
-          <p>Member 4</p>
-        </div>
-      </div>
+      <players v-if="showPlayers" />
+      <my-progress v-if="!showPlayers" />
     </div>
   </div>
 </template>
 
 <script>
+import players from '../components/Leaderboard/Players.vue';
+import myProgress from '../components/Leaderboard/My progress.vue';
+
 export default {
   name: 'leaderboard',
+  components: {
+    players,
+    myProgress,
+  },
   data() {
     return {
       showPlayers: Boolean,
@@ -129,59 +102,5 @@ export default {
     width: 20%;
     height: auto;
     display: inline-block;
-}
-
-.rows {
-   padding-top: 7vh;
-}
-
-.row {
-  display: flex;
-  justify-content: space-evenly;
-  border-bottom: 1px solid #9AC356;
-  .rank {
-    font-weight: bold;
-  }
-}
-
-.points {
-  font-style: italic;
-}
-
-.row:last-child {
-  border: none;
-}
-
-.progress h2 {
-  position: absolute;
-  top: 14vh;
-  left: 35vw;
-}
-
-.progress .ranking {
-  position: absolute;
-  top: 20vh;
-  left: 35vw;
-}
-
-.progress .points {
-  position: absolute;
-  top: 15vh;
-  left: 72vw;
-}
-
-.progress img {
-  position: absolute;
-  top: 15vh;
-  left: 8vw;
-}
-
-.progress .members {
-  position: absolute;
-  top: 35vh;
-  left: 8vw;
-  p {
-    margin: 0.5em 0;
-  }
 }
 </style>
