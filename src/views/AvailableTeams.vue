05695a2b93096f5acd1fb0745e1efac3a8429b61
florisdobber <florisdobber@gmail.com>
2020-05-13 21:47:03 +0200

    Added a route to the available teams page and created the page

---
+++ src/views/AvailableTeams.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <h1>Available Teams</h1>
+      <p>You do not currently belong to any team</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'availableTeams',
+};
+</script>
+
+<style scoped>
+
+</style>
