806d5f33e04d91a7cb26146b2f5a722d23e1f4cc
will <willmt80@gmail.com>
2020-06-02 14:50:43 -0700

    changed mapview styling and removed styling from the map itself

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -24,6 +24,7 @@
     </div>
 
     <Map
+      class="map-container"
       v-bind:reservedFilter="this.reservedFilter"
       v-bind:pushStreet="this.pushStreet"
       v-bind:activeStreetFid="this.activeStreetFid"/>
@@ -169,11 +170,9 @@ export default {
 .header-bar {
   display: flex;
   justify-content: space-evenly;
-  width: 100%;
-  margin: auto;
+  width: auto;
   background-color: #9AC356;
-  padding: 5px;
-  margin: 10px;
+  margin-bottom: 5px;
 }
 
 .streets-container {
@@ -188,8 +187,19 @@ export default {
 
 @media only screen and (max-width: 700px) {
   .streets-container {
-    width: 100%;
+    width: auto;
     margin-left: 5px;
+  }
+}
+
+.map-container {
+    width: 95vw;
+    height: 50vh;
+}
+
+@media only screen and (max-width: 700px) {
+  .map-container {
+    width: 100%;
   }
 }
 </style>
