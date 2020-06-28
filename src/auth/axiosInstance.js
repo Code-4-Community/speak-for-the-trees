b26213b3e6144830f1edbb7d28def7a836fc8f23
Jack Blanc <jblanc222@gmail.com>
2020-06-28 16:07:27 -0700

    porting auth changes from LLB to SFTT

--- src/auth/axiosInstance.js
+++ src/auth/axiosInstance.js
@@ -1,11 +1,10 @@
-/* eslint-disable no-console */
 import axios from 'axios';
 import tokenService from './token';
-import { refresh } from './authAPI';
+import { refresh, logout } from './authAPI';
 
 const AxiosInstance = axios.create({
   baseURL: process.env.VUE_APP_API_DOMAIN,
-  timeout: 5000,
+  timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
   },
@@ -23,16 +22,18 @@ AxiosInstance.interceptors.response.use(
   response => response,
   (error) => {
     const originalRequest = error.config;
-    if (error?.response?.status === 401
-    && error?.response?.data === INVALID_ACCESS_TOKEN
-    && !originalRequest.retry) {
-      originalRequest.retry = true;
+    if (error.response.status === 401
+      && error.response.data === INVALID_ACCESS_TOKEN
+      && tokenService.isRefreshTokenVaid()) {
       return refresh().then(() => {
         AxiosInstance.defaults.headers['X-Access-Token'] = tokenService.getAccessToken();
         return AxiosInstance(originalRequest);
       });
-    }
-    return Promise.reject(error);
+    } if (error.response.status === 401
+      && error.response.data === INVALID_ACCESS_TOKEN
+      && !tokenService.isRefreshTokenVaid()) {
+      logout();
+    } return Promise.reject(error);
   },
 );
 
