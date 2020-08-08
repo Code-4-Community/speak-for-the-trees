87d70267876031eeea76dce599eedbafad415d89
florisdobber <florisdobber@gmail.com>
2020-08-08 10:54:03 -0400

    New fix for forgot password route

--- src/router/index.js
+++ src/router/index.js
@@ -129,8 +129,9 @@ const router = new VueRouter({
 });
 
 router.beforeEach((to, from, next) => {
-  if (tokenService.getPrivilegeLevel() < 0) {
-    if (to.name === 'Login' || to.name === 'SignUp' || to.path === '/forgot-password-reset/*') next();
+  if (tokenService.getPrivilegeLevel() === privilegeConstants.NONE) {
+    if (to.path === '/forgot-password-reset/*') next();
+    else if (to.name === 'Login' || to.name === 'SignUp') next();
     else next({ name: 'Login' });
   } else next();
   if (tokenService.getPrivilegeLevel() !== privilegeConstants.ADMIN) {
