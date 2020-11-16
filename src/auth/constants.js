636105989a4871ce1533effb43ace6cbfb0c95cf
BreeMadison <breannamadison711@gmail.com>
2020-11-16 17:22:31 -0500

    Adding super user level, and changing priv level int valyes

--- src/auth/constants.js
+++ src/auth/constants.js
@@ -2,11 +2,13 @@
 const none = -1;
 const standard = 1;
 const admin = 2;
+const superAdmin = 3;
 
 const privilegeLevel = {
   NONE: none,
   STANDARD: standard,
   ADMIN: admin,
+  SUPER_ADMIN: superAdmin,
 };
 
 export default privilegeLevel;
